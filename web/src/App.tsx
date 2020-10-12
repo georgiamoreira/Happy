f6513ed8194dd3f20d81b1986eeeefc189bb22e4
georgiamoreira <georgiamoreira13@gmail.com>
2020-10-12 16:19:12 -0300

    initial commit

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles/global.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hi Lorena!</h1>
+    </div>
+  );
+}
+
+export default App;
